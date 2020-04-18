07ecd173d29e501eb9013bf7cf392066924d8d1a
Marcel Schoffelmeer <marcel@bluecore.com>
2020-04-18 11:27:27 -0400

    Initial Commit

---
+++ test20200418Tests/test20200418Tests.swift
@@ -0,0 +1,34 @@
+//
+//  test20200418Tests.swift
+//  test20200418Tests
+//
+//  Created by Marcel on 4/18/20.
+//  Copyright © 2020 Marcel. All rights reserved.
+//
+
+import XCTest
+@testable import test20200418
+
+class test20200418Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
